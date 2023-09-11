1fcd2608151c4c6501067c434c8d599362a2d5ae
Gabriel Carvalho <zerowings_@hotmail.com>
2023-09-10 22:26:51 -0300

    commit changes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import rotaUsuario from './Rotas/rotaUsuario.js'
 import rotaDoacao from './Rotas/rotaDoacao.js';
 
 
-// const hostname = "0.0.0.0";
-// const porta = "4019";
+const hostname = "0.0.0.0";
+const porta = "4019";
 
 
 const app = express();
@@ -23,11 +23,11 @@ app.use('/usuarios', rotaUsuario);
 //     console.log(`API rodando em: https://${hostname}:${porta}/doacao`)
 // })
 
-// app.listen(porta, hostname, () => {
-//     console.log(`API rodando em: https://${hostname}:${porta}`)
-// })
+app.listen(porta, hostname, () => {
+    console.log(`API rodando em: https://${hostname}:${porta}`)
+})
 
 
-app.listen(3007, "localhost", () => {
-  console.log("API escutando no link: http://localhost:3007/usuarios");
-});
+// app.listen(3007, "localhost", () => {
+//   console.log("API escutando no link: http://localhost:3007/usuarios");
+// });
