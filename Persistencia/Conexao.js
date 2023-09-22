1dd78d20dbf47e0efbf118b3e1d71fcf17de4683
Gabriel Carvalho <zerowings_@hotmail.com>
2023-09-22 10:04:45 -0300

    fix

--- Persistencia/Conexao.js
+++ Persistencia/Conexao.js
@@ -5,20 +5,20 @@ export default async function conectar() {
     return global.conexao;
   }
 
-  const conexao = await mysql.createConnection({
-    host: "localhost",
-    user: "aluno19-pfsii",
-    port: 3306,
-    password: "aluno19-pfsii",
-    database: "backendaluno19-pfsii",
-  });
-
   // const conexao = await mysql.createConnection({
   //   host: "localhost",
-  //   user: "root",
-  //   password: "",
-  //   database: "backend"
+  //   user: "aluno19-pfsii",
+  //   port: 3306,
+  //   password: "aluno19-pfsii",
+  //   database: "backendaluno19-pfsii",
   // });
+
+  const conexao = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "backend"
+  });
   global.conexao = conexao;
 
   return conexao;
